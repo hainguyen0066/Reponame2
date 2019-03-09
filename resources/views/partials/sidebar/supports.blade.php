bd0a8c703e5bc109248ec34f3c5528546f28135d
Tuan <letuan.bp.1987@gmail.com>
2019-03-09 20:18:25 +0700

    fix nhe giao dien

--- resources/views/partials/sidebar/supports.blade.php
+++ resources/views/partials/sidebar/supports.blade.php
@@ -1,5 +1,5 @@
 <div class="three-btn">
-    <div class="charge"><a href="#"></a></div>
+    <div class="charge"><a href="{{ route('front.payment.index') }}"></a></div>
     <div class="trade"><a href="#"></a></div>
     <div class="info-sv"><a href="#"></a></div>
 </div>
