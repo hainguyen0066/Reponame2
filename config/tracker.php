808cc6909e6a803c57d71f0d531a6e5656a6a455
trungtran <trungtran07@gmail.com>
2019-03-19 13:54:12 +0700

    Update fb chat text

--- config/tracker.php
+++ config/tracker.php
@@ -4,8 +4,8 @@ return [
     'fb' => [
         'chat_popup' => [
             'enabled' => true,
-            'logged_in_greeting' => 'Chào mừng bạn đến Võ Lâm Trùng Phùng, bạn cần mình hỗ trợ gì ạ?',
-            'logged_out_greeting' => 'Chào mừng bạn đến Võ Lâm Trùng Phùng, bạn cần mình hỗ trợ gì ạ?',
+            'logged_in_greeting' => 'Chào mừng bạn đến Võ Lâm Trung Nguyên, bạn cần mình hỗ trợ gì ạ?',
+            'logged_out_greeting' => 'Chào mừng bạn đến Võ Lâm Trung Nguyên, bạn cần mình hỗ trợ gì ạ?',
             'theme_color' => '#0084ff',
             'locale' => 'vi_VN'
         ],
