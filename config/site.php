2ab70d9a6f012496d639a5acedfd3ada022e2dd2
trungtran <trungtran07@gmail.com>
2019-03-20 08:19:25 +0700

    Update fb page info

--- config/site.php
+++ config/site.php
@@ -8,7 +8,7 @@ return [
     ],
     'fb'  => [
         'app_id'   => '1041939975989453',
-        'page_id'  => '334751893828311',
+        'page_id'  => '500304066823669',
         'page_url' => 'https://www.facebook.com/volamtrungnguyen2019/',
         'group_url' => 'https://www.facebook.com/groups/vltrungnguyen'
     ],
