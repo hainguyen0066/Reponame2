64398839c0a2265d402c3c35c412e7bd5c0600fd
trungtran <trungtran07@gmail.com>
2019-04-18 16:52:24 +0700

    Fix date display in user registered chart

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -83,7 +83,7 @@ class UserRepository extends AbstractEloquentRepository
     {
         $fromDate = strtotime($fromDate);
         $toDate = strtotime($toDate);
-        $data = \DB::table('users')->selectRaw("DATE_FORMAT(created_at, '%e-%m') as `date`, CONCAT(utm_campaign, '.', utm_medium, '.', utm_source) as `cid`, DATE_FORMAT(created_at, '%m-%e') as ordered_date, COUNT(id) as `total`")
+        $data = \DB::table('users')->selectRaw("DATE_FORMAT(created_at, '%d-%m') as `date`, CONCAT(utm_campaign, '.', utm_medium, '.', utm_source) as `cid`, DATE_FORMAT(created_at, '%m-%d') as ordered_date, COUNT(id) as `total`")
             ->whereRaw("UNIX_TIMESTAMP(CONVERT_TZ(created_at, '+07:00', '+00:00')) BETWEEN {$fromDate} AND $toDate")
             ->groupBy('date', 'ordered_date', 'cid')
             ->orderByRaw("ordered_date ASC, total DESC")
