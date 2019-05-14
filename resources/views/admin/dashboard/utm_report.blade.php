5fcb7d8c3f608b928cb1e55e9bad14e0c0ae49e2
trungtran <trungtran07@gmail.com>
2019-05-14 16:56:03 +0700

    Fix dashboard display for UTM report

--- resources/views/admin/dashboard/utm_report.blade.php
+++ resources/views/admin/dashboard/utm_report.blade.php
@@ -24,20 +24,20 @@
             <td><?php echo str_replace('|', ' / ', $group) ?> <span class="label label-primary group-count"></span></td>
             <?php endforeach; ?>
             <?php foreach ($dateArray as $date):
-            $key = "{$campaign}.{$group}";
+            $key = "{$campaign}|{$group}";
             $total = isset($data[$date]['details'][$key]) ? $data[$date]['details'][$key] : 0;
             ?>
             <td class="number"><?php echo $total ?></td>
             <?php endforeach; ?>
         </tr>
         <?php else: ?>
-        <tr><td rowspan="<?php echo count($campaigns[$campaign]) ?>"><?php echo $campaign ?></td></tr>
+        <tr><td rowspan="<?php echo count($campaigns[$campaign]) + 1 ?>"><?php echo $campaign ?></td></tr>
         <?php foreach ($campaigns[$campaign] as $group):
         ?>
         <tr>
             <td><?php echo str_replace('|', ' / ', $group) ?> <span class="label label-primary group-count"></span></td>
             <?php foreach ($dateArray as $date):
-            $total = isset($data[$date]['details']["{$campaign}.{$group}"]) ? $data[$date]['details']["{$campaign}.{$group}"] : 0;
+            $total = isset($data[$date]['details']["{$campaign}|{$group}"]) ? $data[$date]['details']["{$campaign}|{$group}"] : 0;
             ?>
             <td class="number"><?php echo $total ?></td>
             <?php endforeach; ?>
