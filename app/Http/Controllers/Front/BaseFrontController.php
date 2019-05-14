f186e3d49342f14ea7f0cb127fc3f9246c17d6ac
trungtran <trungtran07@gmail.com>
2019-05-14 10:30:48 +0700

    Add referer as UTMs

--- app/Http/Controllers/Front/BaseFrontController.php
+++ app/Http/Controllers/Front/BaseFrontController.php
@@ -20,17 +20,19 @@ class BaseFrontController extends Controller
     }
 
     /**
-     * @param      $name
+     * @param $name
+     *
+     * @return bool
      */
     private function track($name)
     {
         $value = request($name);
         if (!$value) {
-            return;
+            return false;
         }
-        $expire = 60*24*7; // 7 days
-        $cookie = Cookie::make($name, $value, $expire);
-        Cookie::queue($cookie);
+        $this->setTrackingCookie($name, $value);
+
+        return true;
     }
 
     /**
@@ -38,8 +40,23 @@ class BaseFrontController extends Controller
      */
     private function tracks(array $names)
     {
+        $tracked = false;
         foreach ($names as $name) {
-            $this->track($name);
+            $tracked = $this->track($name);
         }
+        if (!$tracked && !empty($_SERVER['HTTP_REFERER'])) {
+            $referer = parse_url($_SERVER['HTTP_REFERER']);
+            if (!empty($referer['host'])) {
+                $this->setTrackingCookie('utm_source', $referer['host']);
+                $this->setTrackingCookie('utm_medium', 'Referral');
+            }
+        }
+    }
+
+    private function setTrackingCookie($name, $value)
+    {
+        $expire = 60 * 24 * 7; // 7 days
+        $cookie = Cookie::make($name, $value, $expire);
+        Cookie::queue($cookie);
     }
 }
