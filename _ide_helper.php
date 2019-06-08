782b791427c5830e96c9544ef6d9661003a0bce1
trungtran <trungtran07@gmail.com>
2019-06-08 15:59:16 +0700

    upgrade tcg/voyager

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2019-05-19 15:48:45.
+ * Generated for Laravel 5.7.3 on 2019-06-08 15:59:01.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
