cdef96c7d6be9a1979767dedba56e3c93d46254e
trungtran <trungtran07@gmail.com>
2019-08-15 11:37:15 +0700

    update ladning page

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -56,10 +56,14 @@
         </div>
         <div class="section-05">
             <div class="container">
-                <div class="volamtranhba"><a href="{{ route('front.details.post', ['tong-hop','su-kien-ba-chu-vo-lam'])}}"></a></div>
-                <div class="quanhunghoitu"><a href="{{ route('front.details.post', ['tong-hop','chuoi-su-kien-close-beta-20-03'])}}"></a></div>
-                <div class="anhhungthiep"><a href="https://www.facebook.com/volamtrungnguyen2019/videos/283765432499493" target="_blank"></a></div>
-                <div class="denhatbanghoi"><a href="{{ route('front.details.post', ['tong-hop','de-nhat-bang-hoi'])}}"></a></div>
+                @php
+                $urlServerBaLangHuyen = route('front.details.post', ['tong-hop','chuoi-su-kien-close-beta-18-06-khai-mo-server-ba-l-ng-huyen']);
+                @endphp
+                <div class="volamtranhba"><a href="{{ route('front.details.post', ['tong-hop','su-kien-dua-top-may-chu-ba-l-ng-huyen'])}}"></a></div>
+                <div class="quanhunghoitu"><a href="{{ $urlServerBaLangHuyen }}"></a></div>
+                <div class="anhhungthiep"><a href="{{ $urlServerBaLangHuyen }}"></a></div>
+                <div class="denhatbanghoi"><a href="{{ $urlServerBaLangHuyen }}"></a></div>
+                <div class="code"><a href="{{ $urlServerBaLangHuyen }}"></a></div>
             </div>
         </div>
         <div class="footer">
