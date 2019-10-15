63fe9ef7b029e9a669735cdd046604e1c09e9c66
trungtran <trungtran07@gmail.com>
2019-10-15 15:19:55 +0700

    Upgrade t2g/common to fix CCU chart error.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2019-10-15 14:36:08.
+ * Generated for Laravel 5.7.3 on 2019-10-15 15:19:27.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
