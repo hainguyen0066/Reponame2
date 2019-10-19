53d000041467388c60aeb71be2eb65baa0f9b7cd
trungtran <trungtran07@gmail.com>
2019-10-19 22:50:56 +0700

    Update CCU report chart. Update CCU every 5 minutes.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2019-10-17 17:33:06.
+ * Generated for Laravel 5.7.3 on 2019-10-19 22:44:31.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
