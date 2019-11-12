1c20e5ab716e620614b551f3c235fcc172bf0c29
trungtran <trungtran07@gmail.com>
2019-11-12 23:59:57 +0700

    Update t2g/common for better JX API logs. Add config for skip cashout alert when receiving SMS.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2019-11-07 17:11:06.
+ * Generated for Laravel 5.7.3 on 2019-11-12 23:54:10.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
