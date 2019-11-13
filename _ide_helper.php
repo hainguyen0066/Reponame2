3f9f654f5ab5bf08082e7f9028cbebf47b1be092
trungtran <trungtran07@gmail.com>
2019-11-13 14:50:36 +0700

    Update t2g/common for better JX API error alert.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2019-11-12 23:54:10.
+ * Generated for Laravel 5.7.3 on 2019-11-13 14:49:36.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
