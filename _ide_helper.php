51e103367976a1ff03ae9d3f61004e7ef3c4371f
trungtran <trungtran07@gmail.com>
2019-11-15 15:53:26 +0700

    Support quick revert to a revision of user.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2019-11-13 14:49:36.
+ * Generated for Laravel 5.7.3 on 2019-11-15 15:52:24.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
