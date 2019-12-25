0be6ab093e1fc879499445cb16287ff001c16d2c
trungtran <trungtran07@gmail.com>
2019-12-25 15:55:41 +0700

    Update new logo

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -8,6 +8,12 @@
 <link rel="dns-prefetch" href="//connect.facebook.net"/>
 <title>{{ $title }}</title>
 <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="512x512"  href="/android-chrome-512x512.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<meta name="theme-color" content="#ffffff">
 <meta property="og:title" itemprop="headline" content="{{ $title }}"/>
 <meta property="fb:app_id" content="{{ config('site.fb.app_id') }}"/>
 <meta property="og:description" content="{{ $meta_description }}"/>
