ab7bc7ede2f89f615b6b5a843eb16088ae585ee8
trungtran <trungtran07@gmail.com>
2020-01-11 19:41:13 +0700

    Fix duplicate warning message on running MonitorJXGoldCommand.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-01-09 18:00:16.
+ * Generated for Laravel 5.7.3 on 2020-01-11 19:26:56.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
