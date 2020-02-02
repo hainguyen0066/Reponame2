de908a2b4eff337476a1bebcf6e1e87da629bf19
trungtran <trungtran07@gmail.com>
2020-02-02 20:00:49 +0700

    Backup MySQL database hourly.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('t2g_common:momo:notifier')->everyFiveMinutes();
-        $schedule->command('t2g_common:mysql:backup mysql')->twiceDaily();
+        $schedule->command('t2g_common:mysql:backup mysql')->hourly();
         $schedule->command('t2g_common:ccu:update')->everyFiveMinutes();
         $schedule->command('t2g_common:users:update_last_login')->dailyAt("02:00");
         $schedule->command('t2g_common:monitor:gold')->everyFiveMinutes();
