0b96c569d50d0c009e64e79df262aebd125ea04c
trungtran <trungtran07@gmail.com>
2020-03-03 21:49:44 +0700

    Fix excluding accounts from monitor multiple login.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-03-03 00:03:46.
+ * Generated for Laravel 5.7.3 on 2020-03-03 21:49:03.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
