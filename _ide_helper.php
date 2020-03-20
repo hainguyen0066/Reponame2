f25e321ca0213c8e51643ad485591406fc76a596
trungtran <trungtran07@gmail.com>
2020-03-20 13:18:06 +0700

    Update t2g_common.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-03-19 23:01:04.
+ * Generated for Laravel 5.7.3 on 2020-03-20 13:16:07.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
