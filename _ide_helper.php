b5d7b65660f49bf79da604cc356d9f80006359c7
trungtran <trungtran07@gmail.com>
2020-04-03 18:21:22 +0700

    Add quick edit for Post Grouping.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-04-03 17:15:37.
+ * Generated for Laravel 5.7.3 on 2020-04-03 18:18:48.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
