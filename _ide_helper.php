339679ecfcf55a4087e537cfba83f14c2076d7f3
trungtran <trungtran07@gmail.com>
2020-04-07 16:00:55 +0700

    Change HWID detection mechanism.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-04-05 17:53:07.
+ * Generated for Laravel 5.7.3 on 2020-04-07 15:50:37.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
---
+++ resources/views/partials/post_group.blade.php
@@ -0,0 +1,48 @@
+@if($post->hasGroup() && count($groupPosts) > 0)
+    <ul class="menu-news">
+        @foreach($groupPosts as $item)
+            <li>
+                @if(empty($item['subs']))
+                    <a class="tab-link {{ $item['post']->id == $post->id ? 'active' : '' }}"
+                       title="{{ $item['post']->group_title }}"
+                       href="{{ route('front.details.post', [$item['post']->getCategorySlug(), $item['post']->slug]) }}">{{ $item['post']->group_title }}</a>
+                @else
+                    <a class="tab-link has-subs"
+                       href="javascript:"
+                       title="{{ $item['sub_title'] }}"
+                       data-group="{{ $item['sub_title'] }}">{{ $item['sub_title'] }}</a>
+                @endif
+            </li>
+        @endforeach
+    </ul>
+    <div class="menu-subs">
+        @foreach($groupPosts as $item)
+            @if(!empty($item['subs']))
+                <ul data-group="{{ $item['sub_title'] }}">
+                    @foreach($item['subs'] as $subItem)
+                        @php
+                            $activeClass = $subItem->id == $post->id ? 'active' : '';
+                        @endphp
+                        <li class="{{ $activeClass }}">
+                            <a href="{{ route('front.details.post', [$subItem->getCategorySlug(), $subItem->slug]) }}"
+                               title="{{ $subItem->group_title }}"
+                            >
+                                {{ $subItem->group_title }}
+                            </a>
+                        </li>
+                        @if(!empty($activeClass))
+                            @push('extra-js')
+                                <script>
+                                    activeSub = $('.menu-subs li.active');
+                                    activeSub.parent().addClass('active');
+                                    $('.menu-news .tab-link[data-group="'+ activeSub.parent().data('group') +'"]').addClass('active');
+                                </script>
+                            @endpush
+                        @endif
+                    @endforeach
+                    <div class="clearfix"></div>
+                </ul>
+            @endif
+        @endforeach
+    </div>
+@endif
