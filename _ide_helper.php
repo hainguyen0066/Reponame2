aa73d39a93da2f9fee2efa2e3c7a4a4e63e8e83d
trungtran <trungtran07@gmail.com>
2020-05-01 13:58:20 +0700

    Fix SMSParser on VCB message.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-05-01 13:35:26.
+ * Generated for Laravel 5.7.3 on 2020-05-01 13:57:31.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
