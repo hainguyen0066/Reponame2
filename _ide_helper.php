4f764f3508abe82f5212b4029937ead4d93c8298
trungtran <trungtran07@gmail.com>
2020-07-11 21:24:42 +0700

    Update activities.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.3 on 2020-06-10 14:46:28.
+ * Generated for Laravel 5.7.3 on 2020-07-11 11:27:14.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- resources/views/partials/sidebar/activity.blade.php
+++ resources/views/partials/sidebar/activity.blade.php
@@ -1,75 +1,151 @@
 <div class="act-daily">
     <div class="activity-tabs">
-        <a href="javascript:;" class="link-tab-daily active" data-tab="tab-daily">Hằng ngày</a>
-        <a href="javascript:;" class="link-tab-activity" data-tab="tab-activity">Hoạt động</a>
+        @php
+          $slugs = [
+                'Phong Lăng Độ' => ['huong-dan','phong-l-ng-do'],
+                'Vượt Ải' => ['huong-dan','tinh-n-ng-v-ot-ai-thach-thuc-thoi-gian'],
+                'Quả Huy Hoàng (Tiểu)' => ['huong-dan','hai-qua-huy-hoang'],
+                'Chiến Trường Tống Kim' => ['huong-dan','chien-tr-ong-tong-kim'],
+                'Liên Đấu' => ['huong-dan','lien-dau'],
+                'Quả Huy Hoàng' => ['huong-dan','hai-qua-huy-hoang'],
+                'Boss Đại Hoàng Kim' => ['su-kien','boss-dai-hoang-kim'],
+                'Boss Tiểu Hoàng Kim' => ['huong-dan','boss-tieu-hoang-kim'],
+                'Vận Tiêu' => ['huong-dan','trung-nguyen-tieu-cuc-tinh-n-ng-van-tieu'],
+                'Trung Nguyên Loạn Chiến' => ['huong-dan','trung-nguyen-loan-chien'],
+                'Quốc Chiến Thiên Tử' => ['huong-dan','quoc-chien-thien-tu'],
+                'Thất Thành Đại Chiến'=> ['huong-dan','that-thanh-dai-chien'],
+                'Thương Nhân Tây Vực' => ['huong-dan', 'tinh-n-ng-th-ng-nhan-tay-vuc'],
+            ];
+            $activities = [
+                'T2' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ],
+                'T3' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Vận Tiêu' => '19:00'],
+                    ['Thương Nhân Tây Vực' => '19:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ],
+                'T4' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Trung Nguyên Loạn Chiến' => '20:00'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ],
+                'T5' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ],
+                'T6' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Thương Nhân Tây Vực' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Liên Đấu' => '20:00'],
+                    ['Thất Thành Đại Chiến' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ],
+                'T7' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Liên Đấu' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ],
+                'CN' => [
+                    ['Phong Lăng Độ' => 'Đầu giờ chẵn'],
+                    ['Vượt Ải' => 'Đầu mỗi giờ'],
+                    ['Chiến Trường Tống Kim' => '11:00'],
+                    ['Boss Tiểu Hoàng Kim' => '12:30'],
+                    ['Quả Huy Hoàng (Tiểu)' => '12:30'],
+                    ['Chiến Trường Tống Kim' => '15:00'],
+                    ['Liên Đấu' => '18:00'],
+                    ['Quả Huy Hoàng' => '19:00'],
+                    ['Boss Đại Hoàng Kim' => '19:30'],
+                    ['Boss Tiểu Hoàng Kim' => '20:00'],
+                    ['Liên Đấu' => '20:00'],
+                    ['Trung Nguyên Loạn Chiến' => '20:00'],
+                    ['Quốc Chiến Thiên Tử' => '21:00'],
+                    ['Boss Đại Hoàng Kim' => '22:30'],
+                ]
+            ];
+            $today = date('N');
+            $days = array_keys($activities);
+        @endphp
+        @foreach($days as $index => $day)
+        <a href="javascript:" class="link-tab-daily {{ ($index + 1) == $today ? 'active' : '' }}" data-tab="tab-{{ $day }}">{{ $day }}</a>
+        @endforeach
     </div>
-    <table class="activity-tab-container hide active tab-daily">
+    @foreach($days as $index => $day)
+    <table class="activity-tab-container hide {{ ($index + 1) == $today ? 'active' : '' }} tab-{{ $day }}">
+        @foreach($activities[$day] as $acts)
+            @php
+                $name = array_key_first($acts);
+            @endphp
         <tr>
-            <td class="td-time">Cả ngày</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','phong-l-ng-do'])}}">Phong Lăng Độ</a></td>
+            <td class="td-content">
+                <a href="{{ route('front.details.post', $slugs[$name])}}">{{ $name }}</a>
+            </td>
+            <td class="td-time">{{ $acts[$name] }}</td>
         </tr>
-        <tr>
-            <td class="td-time">Cả ngày</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','tinh-n-ng-v-ot-ai-thach-thuc-thoi-gian'])}}">Vượt ải</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">11:00</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','chien-tr-ong-tong-kim'])}}">Chiến trường tống kim</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">12:30</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','hai-qua-huy-hoang'])}}">Quả huy hoàng (tiểu)</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">12:30</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','boss-tieu-hoang-kim'])}}">Boss tiểu hoàng kim</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">15:00</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','chien-tr-ong-tong-kim'])}}">Chiến trường tống kim</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">19:00</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','hai-qua-huy-hoang'])}}">Quả huy hoàng</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">19:30</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['su-kien','boss-dai-hoang-kim'])}}">Boss Đại Hoàng Kim</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">20:00</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','boss-tieu-hoang-kim'])}}">Boss tiểu hoàng kim</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">21:00</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','chien-tr-ong-tong-kim'])}}">Chiến trường tống kim</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">22:30</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['su-kien','boss-dai-hoang-kim'])}}">Boss Đại Hoàng Kim</a></td>
-        </tr>
+        @endforeach
     </table>
-    <table class="activity-tab-container hide tab-activity">
-        <tr>
-            <td class="td-time">Thứ 2</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','quoc-chien-thien-tu'])}}">Quốc chiến Thiên Tử</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">Thứ 3</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','trung-nguyen-tieu-cuc-tinh-n-ng-van-tieu'])}}">Vận tiêu</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">Thứ 4</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','trung-nguyen-loan-chien'])}}">Trung Nguyên loạn chiến</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">Thứ 6</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','that-thanh-dai-chien'])}}">Thất thành đại chiến</a></td>
-        </tr>
-        <tr>
-            <td class="td-time">Chủ nhật</td>
-            <td class="td-content"><a href="{{ route('front.details.post', ['huong-dan','trung-nguyen-loan-chien'])}}">Trung Nguyên loạn chiến</a></td>
-        </tr>
-    </table>
+    @endforeach
     <div class="act-daily-view-more"><a href="{{ route('front.category', ['huong-dan']) }}">Xem thêm >></a></div>
 </div>
