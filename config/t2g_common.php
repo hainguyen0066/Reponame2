02e6162d554764d0f4275ea965c0488eff08e827
trungtran <trungtran07@gmail.com>
2020-08-29 13:12:38 +0700

    Update share image.

--- config/t2g_common.php
+++ config/t2g_common.php
@@ -11,7 +11,7 @@ return [
             'title'            => 'Võ Lâm Trung Nguyên',
             'meta_keyword'     => 'vo lam trung nguyen, vltk hay, game pk hay, volam1, ctc, vo lam ctc, vo lam cong thanh chien, vo lam 1',
             'meta_description' => 'Khai mở máy chủ Lâm An - 19h00 ngày 11/07/2020. Máy Chủ Cực Mạnh - Miễn Phí Giờ Chơi - Khơi Lại Ký Ức.',
-            'meta_image'       => 'images/share_20200627.jpg',
+            'meta_image'       => 'images/share_202008.jpg',
         ],
         // Open Graph configs
         'og' => [
