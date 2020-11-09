976810993be97756c46d7e9cadc9c0834b25d136
trungtran <trungtran07@gmail.com>
2020-11-09 12:16:34 +0700

    Update excluded accounts.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.23.0 on 2020-09-25 16:35:56.
+ * Generated for Laravel 7.23.0 on 2020-11-09 12:15:15.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
