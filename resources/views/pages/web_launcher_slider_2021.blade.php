27a9d18231e4824aff04518041196e0d02ab1b39
trungtran <trungtran07@gmail.com>
2021-03-09 15:50:24 +0700

    fix mix error

--- resources/views/pages/web_launcher_slider_2021.blade.php
+++ resources/views/pages/web_launcher_slider_2021.blade.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }} ">
-    <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }} ">
-    <link rel="stylesheet" href="{{ asset('css/slick.css') }} ">
-    <link rel="stylesheet" href="{{ asset('css/web_launcher_2021.css') }} ">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}?v={{ config('t2g_common.asset.version') }}">
+    <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}?v={{ config('t2g_common.asset.version') }}">
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}?v={{ config('t2g_common.asset.version') }}">
+    <link rel="stylesheet" href="{{ asset('css/web_launcher_2021.css') }}?v={{ config('t2g_common.asset.version') }}">
 </head>
 <body>
 <div class="wrapper-slider">
@@ -20,8 +20,8 @@
         </section>
     @endif
 </div>
-<script type="text/javascript" src="{{ asset('js/launcher/jquery-3.5.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/launcher/slick.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/launcher/web_laucher.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/launcher/jquery-3.5.js') }}?v={{ config('t2g_common.asset.version') }}"></script>
+<script type="text/javascript" src="{{ asset('js/launcher/slick.js') }}?v={{ config('t2g_common.asset.version') }}"></script>
+<script type="text/javascript" src="{{ asset('js/launcher/web_laucher.js') }}?v={{ config('t2g_common.asset.version') }}"></script>
 </body>
 </html>
