18b72235709f242a8b8531f79b73e9c0f58764fb
Hai Nguyen <hainguyen@t2gcorp.com>
2021-03-10 15:03:57 +0700

    commit feature/#631-thay-doi-web-laucher-cho-c02  * thay đổi thành jquery cdn 1.12.0

--- resources/views/pages/web_launcher_slider_2021.blade.php
+++ resources/views/pages/web_launcher_slider_2021.blade.php
@@ -17,7 +17,7 @@
                 </section>
             @endif
         </div>
-        <script type="text/javascript" src="{{ staticUrl('js/launcher/jquery-3.5.js', true) }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js" integrity="sha512-K7Zj7PGsHk2fpY3Jwvbuo9nKc541MofFrrLaUUO9zqghnJxbZ3Zn35W/ZeXvbT2RtSujxGbw8PgkqpoZXXbGhw==" crossorigin="anonymous"></script>
         <script type="text/javascript" src="{{ staticUrl('js/launcher/slick.js', true) }}"></script>
         <script type="text/javascript" src="{{ staticUrl('js/launcher/web_laucher.js', true) }}"></script>
     </body>
