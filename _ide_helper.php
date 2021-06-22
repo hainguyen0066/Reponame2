bf8328850565813b5e286b20899562611f16d4a1
Aaron Van <ducvinh.van@gmail.com>
2021-06-22 11:19:13 +0700

    + use comom mater

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.23.0 on 2021-06-16 11:19:39.
+ * Generated for Laravel 7.23.0 on 2021-06-22 11:17:30.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
