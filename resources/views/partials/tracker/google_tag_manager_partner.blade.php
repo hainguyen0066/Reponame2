03df5111546ea3e24598f9951f7b4911496dbdb7
Duc Vinh Van <ducvinh.van@gmail.com>
2021-10-01 12:15:44 +0700

    + config GMT, GA, AW, FB pixels

--- resources/views/partials/tracker/google_tag_manager_partner.blade.php
+++ resources/views/partials/tracker/google_tag_manager_partner.blade.php
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Ads: 772272124 --> 
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-772272124"></script> 
 <script> 
-    window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-772272124'); 
+    window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-447403547'); 
 </script>
